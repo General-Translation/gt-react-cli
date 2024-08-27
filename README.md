05f71b5f23a219f5640c583f3af562563706be86
archie-mckenzie <archie@generaltranslation.com>
2024-08-26 20:40:07 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# gt-cli
+
+A CLI tool for processing React dictionary files and sending translations to General Translation services.
+
+## Installation
+
+### Global Installation
+
+To install the `i18n` CLI tool globally using npm, run:
+
+```bash
+npm install -g gt-cli
+```
+
+This will make the `i18n` command available globally on your system.
+
+## Usage
+
+### Update Command
+
+The primary command is `update`. This command processes a React dictionary file and sends the translations to General Translation services.
+
+#### Basic Usage
+
+Once installed globally, you can run the command:
+
+```bash
+i18n update dictionary.js --apiKey YOUR_GT_API_KEY --projectID YOUR_GT_PROJECT_ID --languages en fr es --override
+```
+
+- `dictionary.js`: Path to the dictionary file you want to process.
+- `--apiKey`: Your General Translation API key.
+- `--projectID`: Your General Translation project ID.
+- `--languages`: A list of languages you want to target for translation.
+- `--override`: (Optional) Specify this flag to override existing translations.
+
+### Example
+
+```bash
+i18n update ./src/dictionary.js --apiKey YOUR_GT_API_KEY --projectID YOUR_GT_PROJECT_ID --languages en fr de --override
+```
+
+This command will process the `dictionary.js` file, target English, French, and German for translation, and send the results to the General Translation service.
+
+### Default Paths
+
+If you do not provide a dictionary file path, the tool will look for a dictionary file in the following default locations:
+
+- `./dictionary.js`
+- `./dictionary.jsx`
+- `./dictionary.ts`
+- `./dictionary.tsx`
+- `./src/dictionary.js`
+- `./src/dictionary.jsx`
+- `./src/dictionary.ts`
+- `./src/dictionary.tsx`
+
+If no file is found, the command will throw an error.
+
+## Environment Variables
+
+This tool also supports environment variables for the API key and project ID. You can define them in `.env` or `.env.local` files:
+
+```env
+GT_API_KEY=your_api_key
+GT_PROJECT_ID=your_project_id
+```
+
+If these environment variables are set, you do not need to provide the `--apiKey` and `--projectID` flags in the command.
+
+## License
+
+ISC
---
+++ bin/i18n.js
@@ -0,0 +1,165 @@
+#!/usr/bin/env node
+
+require('dotenv').config({ path: '.env' });
+require('dotenv').config({ path: '.env.local', override: true });
+const path = require('path');
+const mockRequire = require('mock-require');
+const React = require('react');
+const { program } = require('commander');
+const { flattenDictionary, writeChildrenAsObjects, addGTIdentifier } = require('gt-react');
+const { default: GT, getLanguageName, isValidLanguageCode, getLanguageCode } = require('generaltranslation');
+const fs = require('fs');
+
+mockRequire('server-only', () => { 
+    console.log('Mocking server-only module');
+    return {}; 
+});
+
+require('@babel/register')({
+    presets: [
+        ['@babel/preset-react', { runtime: 'automatic' }],
+        '@babel/preset-env'
+    ],
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    ignore: [/(node_modules)/],
+});
+
+function processDictionaryFile(dictionaryFilePath, options) {
+    const absoluteDictionaryFilePath = path.resolve(dictionaryFilePath);
+
+    let dictionary;
+    try {
+        dictionary = require(absoluteDictionaryFilePath).default || require(absoluteDictionaryFilePath);
+    } catch (error) {
+        console.error('Failed to load the dictionary file:', error);
+        process.exit(1);
+    }
+
+    dictionary = flattenDictionary(dictionary);
+
+    const apiKey = options.apiKey || process.env.GT_API_KEY;
+    const projectID = options.projectID || process.env.GT_PROJECT_ID;
+    const dictionaryName = options.dictionaryName;
+    const languages = (options.languages || [])
+        .map(language => isValidLanguageCode(language) ? language : getLanguageCode(language))
+        .filter(language => language ? true : false);
+    const override = options.override ? true : false;
+    if (!(apiKey && projectID)) {
+        throw new Error('GT_API_KEY and GT_PROJECT_ID environment variables or provided arguments are required.');
+    }
+
+    let templateUpdates = [];
+    for (const key in dictionary) {
+        let entry = dictionary[key];
+        let metadata = { projectID, id: key };
+        if (dictionaryName) {
+            metadata.dictionaryName = dictionaryName;
+        }
+        let props = {};
+        if (Array.isArray(entry)) {
+            if (typeof entry[1] === 'object') {
+                props = entry[1];
+            }
+            entry = entry[0];
+        }
+        if (React.isValidElement(entry)) {
+            let wrappedEntry;
+            const { singular, plural, dual, zero, one, two, few, many, other, ranges, ...tMetadata } = props;
+            const pluralProps = Object.fromEntries(
+                Object.entries({ singular, plural, dual, zero, one, two, few, many, other, ranges }).filter(([_, value]) => value !== undefined)
+            );
+            if (Object.keys(pluralProps).length) {
+                const Plural = (pluralProps) => React.createElement(React.Fragment, pluralProps, entry);
+                (Plural).gtTransformation = 'plural';
+                wrappedEntry = React.createElement(Plural, pluralProps, entry);
+            } else {
+                wrappedEntry = React.createElement(React.Fragment, null, entry);
+            };
+            console.log(wrappedEntry)
+            const entryAsObjects = writeChildrenAsObjects(addGTIdentifier(wrappedEntry)); // simulate gt-react's t() function
+            console.log(entryAsObjects)
+            templateUpdates.push({
+                override,
+                type: "react",
+                data: {
+                    children: entryAsObjects,
+                    targetLanguages: languages,
+                    metadata: { ...metadata, ...tMetadata }
+                }
+            });
+            
+        } else if (typeof entry === 'string') {
+            templateUpdates.push({
+                override,
+                type: "intl",
+                data: {
+                    content: entry,
+                    targetLanguages: languages,
+                    metadata: { ...metadata, ...props }
+                }
+            });
+        }
+    }
+
+    if (templateUpdates.length) {
+        const gt = new GT({ apiKey, projectID });
+        const sendUpdates = async () => {
+            const resultLanguages = await gt.updateRemoteDictionary(templateUpdates);
+            if (resultLanguages) {
+                console.log(
+                    `Remote dictionary updated: ${resultLanguages.length ? true : false}.`,
+                    (`Languages: ${resultLanguages.length ? `[${resultLanguages.map(language => `"${getLanguageName(language)}"`).join(', ')}]` + '.' : 'None.'}`),
+                    resultLanguages.length ? 'Translations are usually live within a minute.' : '',
+                );
+            } else {
+                throw new Error('500: Internal Server Error.');
+            }
+            process.exit(0);
+        };
+        sendUpdates();
+    }
+
+    setTimeout(() => {
+        process.exit(0);
+    }, 4000);
+}
+
+function resolveFilePath(filePath, defaultPaths) {
+    if (filePath) {
+        return filePath;
+    }
+
+    for (const possiblePath of defaultPaths) {
+        if (fs.existsSync(possiblePath)) {
+            return possiblePath;
+        }
+    }
+
+    throw new Error('File not found in default locations.');
+}
+
+program
+    .name('update')
+    .description('Process React dictionary files and send translations to General Translation services')
+    .version('1.0.0')
+    .argument('[dictionaryFilePath]', 'Path to the dictionary file')
+    .option('--apiKey <apiKey>', 'Specify your GT API key')
+    .option('--projectID <projectID>', 'Specify your GT project ID')
+    .option('--dictionaryName <name>', 'Optionally specify a dictionary name for metadata purposes')
+    .option('--languages <languages...>', 'List of target languages for translation')
+    .option('--override', 'Override existing translations')
+    .action((dictionaryFilePath, options) => {
+        const resolvedDictionaryFilePath = resolveFilePath(dictionaryFilePath, [
+            './dictionary.js',
+            './dictionary.jsx',
+            './dictionary.ts',
+            './dictionary.tsx',
+            './src/dictionary.js',
+            './src/dictionary.jsx',
+            './src/dictionary.ts',
+            './src/dictionary.tsx'
+        ]);
+        processDictionaryFile(resolvedDictionaryFilePath, options);
+});
+
+program.parse();
